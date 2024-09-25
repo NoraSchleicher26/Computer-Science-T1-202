2bc70b48cce7217389a274722897c87edd0deb15
noraschleicher <824524@mystma.org>
2024-09-25 09:09:29 -0500

    Sept 25

--- Assigments/animalsurvey.py
+++ Assigments/animalsurvey.py
@@ -1,1 +1,8 @@
-intro = input ("What's your favorite animal?\n-")
+animal = input ("What's your favorite animal?\n-")
+like = input ("Why do you like them?\n-")
+color = input ("What color are they?\n-")
+fact = input ("What is a fun fact about them?\n-")
+eat = input ("What do they eat?\n-")
+live = input ("Where do they live?\n-")
+print ("Your favorite animal is the " + animal + " because " + like +"." + " They are " + color + " and " + fact + "." + " They eat " + eat + " and live in "+ live + ".")
+
