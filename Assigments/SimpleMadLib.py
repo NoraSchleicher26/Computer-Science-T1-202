3304feedf0a03d730edaee4dedb7b7c9557add97
noraschleicher <824524@mystma.org>
2024-09-26 09:24:06 -0500

    MadLibAssignment9/26

---
+++ Assigments/SimpleMadLib.py
@@ -0,0 +1,10 @@
+Place = input("Write a place\n-")       #Asking user for place to fill mad lib
+Adjective = input("Write an adjective\n-")      #Asking user for Adjective to fill mad lib
+Verb = input("Write a verb ending in ing\n-")       #Asking user for Verb to fill mad lib
+Person = input("Write a person's name\n-")      #Asking user for name of person to fill mad lib
+Number = input("Write a number\n-")     # Asking user for random number to fill mad lib
+PluralNoun = input("Write a plural noun\n-")        #Asking user for plural noun to fill mad lib
+Adjective2 = input("Write another adjective\n-")        #Asking user for another adjective to fill mad lib
+Adjective3 = input("Write another adjective\n-")        #Asking user for another adjective to fill mad lib
+Verb2 = input("Write another verb ending in ing\n-")        #Asking user for another verb ending in ing to fill mad lib
+print (" Today I went to " + Place + " and saw a " + Adjective + " racoon " + Verb + " through trash. " + " Then I saw " + Person + " carry " + Number + " " + PluralNoun + " all the way over to the trash can and laugh at the racoon. " + " The " + Number + " " + PluralNoun + " fell in the trash. " + " The " + Adjective2 + " and " + Adjective3 + " raccons were then " + Verb2 + " through the mess. ")        #printing summary of users in answers in fun fill in the blank random mad lib
