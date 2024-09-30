040d9a142b8b5046a5317ad669662a1c09db0b0b
noraschleicher <824524@mystma.org>
2024-09-30 09:52:50 -0500

    simplecalculatorassignment

---
+++ Assigments/simplecalculator.py
@@ -0,0 +1,34 @@
+def add ():
+    print("Add two numbers:")
+    x = input ("What is the first number?\n-")
+    x = int(x)
+    y = input ("What is the second number?\n-")
+    y = int(y)
+    print (str(x) + " + " + str(y) + " = " + str(x + y))
+
+def subtract ():
+    print("Subtract two numbers:")
+    x = input ("What is the first number?\n-")
+    x = int(x)
+    y = input ("What is the second number?\n-")
+    y = int(y)
+    print (str(x) + " - " + str(y) + " = " + str(x - y))
+
+def multiply ():
+    print("Multiply two numbers:")
+    x = input ("What is the first number?\n-")
+    x = int(x)
+    y = input ("What is the second number?\n-")
+    y = int(y)
+    print (str(x) + " * " + str(y) + " = " + str(x * y))
+
+def divide ():
+    print("Divide two numbers:")
+    x = input ("What is the first number?\n-")
+    x = int(x)
+    y = input ("What is the second number?\n-")
+    y= int(y)
+    print (str(x) + " / " + str(y) + " = " + str(x / y))
+  
+
+    
