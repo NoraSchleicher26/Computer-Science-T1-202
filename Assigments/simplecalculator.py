ea877d0c70f3ec0a8355a53afda051e29d32a395
noraschleicher <824524@mystma.org>
2024-10-01 09:48:33 -0500

    october_1

--- Assigments/simplecalculator.py
+++ Assigments/simplecalculator.py
@@ -1,34 +1,34 @@
-def add ():
-    print("Add two numbers:")
-    x = input ("What is the first number?\n-")
-    x = int(x)
-    y = input ("What is the second number?\n-")
-    y = int(y)
-    print (str(x) + " + " + str(y) + " = " + str(x + y))
+def add ():     #defining add function
+    print("Add two numbers:")       #printing statement of what function is doing
+    x = input ("What is the first number?\n-")      #Asking for input for first number
+    x = int(x)      #converting x into integer
+    y = input ("What is the second number?\n-")     #Asking for input for second number
+    y = int(y)      #converting y into integer
+    print (str(x) + " + " + str(y) + " = " + str(x + y))  #printing summary and answer of function for adding
 
-def subtract ():
-    print("Subtract two numbers:")
-    x = input ("What is the first number?\n-")
-    x = int(x)
-    y = input ("What is the second number?\n-")
-    y = int(y)
-    print (str(x) + " - " + str(y) + " = " + str(x - y))
+def subtract ():        #defining subtract function
+    print("Subtract two numbers:")      #printing statement of what function is doing
+    x = input ("What is the first number?\n-")      #Asking for input for first number
+    x = int(x)      #converting x into integer
+    y = input ("What is the second number?\n-")     #Asking for input for second number
+    y = int(y)      #converting y into integer
+    print (str(x) + " - " + str(y) + " = " + str(x - y))  #printing summary and answer of function for subtracting
 
-def multiply ():
-    print("Multiply two numbers:")
-    x = input ("What is the first number?\n-")
-    x = int(x)
-    y = input ("What is the second number?\n-")
-    y = int(y)
-    print (str(x) + " * " + str(y) + " = " + str(x * y))
+def multiply ():    #defining multiply function
+    print("Multiply two numbers:")      #printing statement of what function is doing
+    x = input ("What is the first number?\n-")      #asking for input for first number
+    x = int(x)      #converting x into integer
+    y = input ("What is the second number?\n-")     #Asking for input for second number
+    y = int(y)      #converting y into integer
+    print (str(x) + " * " + str(y) + " = " + str(x * y))        #printing summary and answer of function for multiplying
 
-def divide ():
-    print("Divide two numbers:")
-    x = input ("What is the first number?\n-")
-    x = int(x)
-    y = input ("What is the second number?\n-")
-    y= int(y)
-    print (str(x) + " / " + str(y) + " = " + str(x / y))
+def divide ():      #Defining divide function
+    print("Divide two numbers:")        #printing statement of what function is doing
+    x = input ("What is the first number?\n-")      #asking for input for first number
+    x = int(x)      #converting x into integer
+    y = input ("What is the second number?\n-")     #asking for input for second number
+    y= int(y)       #converting y into integer
+    print (str(x) + " / " + str(y) + " = " + str(x / y))        #printing summary and answer of function for dividing
   
 
     
