9fb710019e3a4596170c3c05bb19a33964ab9a64
noraschleicher <824524@mystma.org>
2024-10-21 09:49:28 -0500

    randomization notes

---
+++ Unit 3 - Conditionals and Exceptions/randomization.py
@@ -0,0 +1,12 @@
+import random
+
+r = random.randrange(0,10)
+# ) is INCLUSIVE and the 10 is EXCLUSIVE
+# 0 <= result < 10
+print(r)
+
+p = random.random()
+if p < 0.25:
+    print("SUCCESS")
+else:
+    print("FAIL")
