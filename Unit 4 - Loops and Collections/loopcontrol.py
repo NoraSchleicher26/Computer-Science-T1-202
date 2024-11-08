38e165a32f199c8b045900bc7613c8f55d23d098
noraschleicher <824524@mystma.org>
2024-11-08 09:11:49 -0600

    loop control

--- Unit 4 - Loops and Collections/loopcontrol.py
+++ Unit 4 - Loops and Collections/loopcontrol.py
@@ -5,7 +5,7 @@
 
 
 #break
-#exits a loop prematurely, before it was supposed to end
+#Exits a loop prematurely, before it was supposed to end
 
 
 #Example
@@ -18,3 +18,33 @@ for fruit in bag_of_fruits:
         break       #the break statement exits the loop immediately and continues
     else:
         print("You ate a " + fruit)
+
+
+#continue
+#Skips the current loop
+#It does not exit the entire loop, just moves on to the next iteration
+        
+orders = [15, 30, 35, 23, 100, 3, 10, 22]
+
+#Only apply discount for orders above 20 dollars
+#Coupon applying program
+for order in orders:
+    if order < 20:
+        continue    #Skips the rest of the loop iteration and goes to the next iteration
+    print(str(order) + " order discounted 5% to $" + str(order * 0.95))
+
+#pass
+#Does nothing
+#Usually used as a placeholder while writing code
+#Text adventure project
+    
+if True:
+    pass 
+def enter_forest():
+    pass
+
+def swim_river():
+    pass
+
+def eat_icecream():
+    pass
