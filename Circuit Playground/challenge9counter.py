c83804f3ca76fd4a2d1f55ed4f26504e8ccfc47f
noraschleicher <824524@mystma.org>
2024-11-19 09:45:23 -0600

    challenge 9 progress

--- Circuit Playground/challenge9counter.py
+++ Circuit Playground/challenge9counter.py
@@ -1,10 +1,21 @@
-from adafruit_circuitplayground import cp       #Importing cp to allow code in playground to run
+from adafruit_circuitplayground import cp     #Importing cp to allow code in playground to run
 import time     #Import time for program to be able to run on time durations
 
-presses = 0
-while True:
-    
-    if cp.button_a:
-        presses = presses + 1
-        lights = [0,1,2,3,4,5,6,7,8,9]
-        for light in lights:
+
+presses = 0       #Sets the variable for presses to -1
+while True:     #Has function run all the time
+
+   
+    if cp.button_a:     #Shows what happens when button A is pressed
+        if presses < 10:
+            cp.pixels[presses] = (1,0,0)        #Turns on pixel variable by presses to red
+            presses = presses + 1       #Increases presses variable by 1
+  
+            time.sleep(0.5)     #Waits 500ms
+
+
+    if cp.button_b:     #Shows what happens when button B is pressed
+
+            cp.pixels[presses] = (0,0,0)        #Turns of pixel variable by presses
+            presses = presses  - 1      #Decreases presses variable by 1
+            time.sleep(0.5)     #Waits 500ms
